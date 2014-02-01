1f0e4b77e65e1ac92551d541e4e4399f7635ccf4
Will Roberts <wildwilhelm@gmail.com>
2014-02-01 12:18:54 +0100

    timeparse: generalise time formats

--- timeparse.py
+++ timeparse.py
@@ -13,19 +13,29 @@ HOURS      = r'(?P<hours>[\d.]+)\s*(?:h|hrs?|hours?)'
 MINS       = r'(?P<mins>[\d.]+)\s*(?:m|(mins?)|(minutes?))'
 SECS       = r'(?P<secs>[\d.]+)\s*(?:s|secs?|seconds?)'
 SEPARATORS = r'[,/]'
+MINCLOCK   = r'(?P<mins>\d{1,2}):(?P<secs>\d{2}(?:\.\d+)?)'
+HOURCLOCK  = r'(?P<hours>\d+):(?P<mins>\d{2}):(?P<secs>\d{2}(?:\.\d+)?)'
 
 OPT    = lambda x: '(?:{x})?'.format(x=x, SEPARATORS=SEPARATORS)
 OPTSEP = lambda x: '(?:{x}\s*(?:{SEPARATORS}\s*)?)?'.format(
     x=x, SEPARATORS=SEPARATORS)
 
-TIME = '{WEEKS}\s*{DAYS}\s*{HOURS}\s*{MINS}\s*{SECS}'.format(
-    #YEARS=OPTSEP(YEARS),
-    #MONTHS=OPTSEP(MONTHS),
-    WEEKS=OPTSEP(WEEKS),
-    DAYS=OPTSEP(DAYS),
-    HOURS=OPTSEP(HOURS),
-    MINS=OPTSEP(MINS),
-    SECS=OPT(SECS))
+TIMEFORMATS = [
+    r'{WEEKS}\s*{DAYS}\s*{HOURS}\s*{MINS}\s*{SECS}'.format(
+        #YEARS=OPTSEP(YEARS),
+        #MONTHS=OPTSEP(MONTHS),
+        WEEKS=OPTSEP(WEEKS),
+        DAYS=OPTSEP(DAYS),
+        HOURS=OPTSEP(HOURS),
+        MINS=OPTSEP(MINS),
+        SECS=OPT(SECS)),
+    r'{MINCLOCK}'.format(
+        MINCLOCK=MINCLOCK),
+    r'{WEEKS}\s*{DAYS}\s*{HOURCLOCK}'.format(
+        WEEKS=OPTSEP(WEEKS),
+        DAYS=OPTSEP(DAYS),
+        HOURCLOCK=HOURCLOCK),
+    ]
 
 def t(x, y):
     if re.match(x, y):
@@ -43,23 +53,24 @@ MULTIPLIERS = dict([
     ])
 
 def timeparse(sval):
-    match = re.match(TIME + r'\s*$', sval)
-    if match and match.group(0).strip():
-        mdict = match.groupdict()
-        # if all of the fields are integer numbers
-        if all(v.isdigit() for v in mdict.values() if v):
-            return sum([MULTIPLIERS[k] * int(v, 10) for (k, v) in
-                        mdict.items() if v is not None])
-        # if SECS is an integer number
-        elif ('secs' not in mdict or
-              mdict['secs'] is None or
-              mdict['secs'].isdigit()):
-            # we will return an integer
-            return (
-                int(sum([MULTIPLIERS[k] * float(v) for (k, v) in
-                         mdict.items() if k != 'secs' and v is not None])) +
-                (int(mdict['secs'], 10) if mdict['secs'] else 0))
-        else:
-            # SECS is a float, we will return a float
-            return sum([MULTIPLIERS[k] * float(v) for (k, v) in
-                        mdict.items() if v is not None])
+    for format in TIMEFORMATS:
+        match = re.match(format + r'\s*$', sval)
+        if match and match.group(0).strip():
+            mdict = match.groupdict()
+            # if all of the fields are integer numbers
+            if all(v.isdigit() for v in mdict.values() if v):
+                return sum([MULTIPLIERS[k] * int(v, 10) for (k, v) in
+                            mdict.items() if v is not None])
+            # if SECS is an integer number
+            elif ('secs' not in mdict or
+                  mdict['secs'] is None or
+                  mdict['secs'].isdigit()):
+                # we will return an integer
+                return (
+                    int(sum([MULTIPLIERS[k] * float(v) for (k, v) in
+                             mdict.items() if k != 'secs' and v is not None])) +
+                    (int(mdict['secs'], 10) if mdict['secs'] else 0))
+            else:
+                # SECS is a float, we will return a float
+                return sum([MULTIPLIERS[k] * float(v) for (k, v) in
+                            mdict.items() if v is not None])
