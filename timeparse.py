c9a025ce33b95a31fa67d31d3ef70f8ace4193ad
Will Roberts <wildwilhelm@gmail.com>
2014-02-01 12:37:01 +0100

    timeparse: documentation, pylint cleanups

--- timeparse.py
+++ timeparse.py
@@ -1,24 +1,53 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
-# ------------------------------------------------------------
-#  implementation
-# ------------------------------------------------------------
+'''
+timeparse.py
+(c) Will Roberts <wildwilhelm@gmail.com>  1 February, 2014
+
+Implements a single function, `timeparse`, which can parse various
+kinds of time expressions.
+'''
+
+# MIT LICENSE
+#
+# Permission is hereby granted, free of charge, to any person
+# obtaining a copy of this software and associated documentation files
+# (the "Software"), to deal in the Software without restriction,
+# including without limitation the rights to use, copy, modify, merge,
+# publish, distribute, sublicense, and/or sell copies of the Software,
+# and to permit persons to whom the Software is furnished to do so,
+# subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+# BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+# ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+# CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
 
 import re
 
-#YEARS     = r'(?P<years>\d+)\s*(?:ys?|yrs?.?|years?)'
-#MONTHS    = r'(?P<months>\d+)\s*(?:mos?.?|mths?.?|months?)'
-WEEKS      = r'(?P<weeks>[\d.]+)\s*(?:w|wks?|weeks?)'
-DAYS       = r'(?P<days>[\d.]+)\s*(?:d|dys?|days?)'
-HOURS      = r'(?P<hours>[\d.]+)\s*(?:h|hrs?|hours?)'
-MINS       = r'(?P<mins>[\d.]+)\s*(?:m|(mins?)|(minutes?))'
-SECS       = r'(?P<secs>[\d.]+)\s*(?:s|secs?|seconds?)'
-SEPARATORS = r'[,/]'
-MINCLOCK   = r'(?P<mins>\d{1,2}):(?P<secs>\d{2}(?:\.\d+)?)'
-HOURCLOCK  = r'(?P<hours>\d+):(?P<mins>\d{2}):(?P<secs>\d{2}(?:\.\d+)?)'
-DAYCLOCK   = r'(?P<days>\d+):(?P<hours>\d{2}):(?P<mins>\d{2}):(?P<secs>\d{2}(?:\.\d+)?)'
+#YEARS      = r'(?P<years>\d+)\s*(?:ys?|yrs?.?|years?)'
+#MONTHS     = r'(?P<months>\d+)\s*(?:mos?.?|mths?.?|months?)'
+WEEKS       = r'(?P<weeks>[\d.]+)\s*(?:w|wks?|weeks?)'
+DAYS        = r'(?P<days>[\d.]+)\s*(?:d|dys?|days?)'
+HOURS       = r'(?P<hours>[\d.]+)\s*(?:h|hrs?|hours?)'
+MINS        = r'(?P<mins>[\d.]+)\s*(?:m|(mins?)|(minutes?))'
+SECS        = r'(?P<secs>[\d.]+)\s*(?:s|secs?|seconds?)'
+SEPARATORS  = r'[,/]'
+MINCLOCK    = r'(?P<mins>\d{1,2}):(?P<secs>\d{2}(?:\.\d+)?)'
+HOURCLOCK   = r'(?P<hours>\d+):(?P<mins>\d{2}):(?P<secs>\d{2}(?:\.\d+)?)'
+DAYCLOCK    = (r'(?P<days>\d+):(?P<hours>\d{2}):'
+               r'(?P<mins>\d{2}):(?P<secs>\d{2}(?:\.\d+)?)')
 
-OPT    = lambda x: '(?:{x})?'.format(x=x, SEPARATORS=SEPARATORS)
-OPTSEP = lambda x: '(?:{x}\s*(?:{SEPARATORS}\s*)?)?'.format(
+OPT         = lambda x: r'(?:{x})?'.format(x=x, SEPARATORS=SEPARATORS)
+OPTSEP      = lambda x: r'(?:{x}\s*(?:{SEPARATORS}\s*)?)?'.format(
     x=x, SEPARATORS=SEPARATORS)
 
 TIMEFORMATS = [
@@ -40,24 +69,39 @@ TIMEFORMATS = [
         DAYCLOCK=DAYCLOCK),
     ]
 
-def t(x, y):
-    if re.match(x, y):
-        print re.match(x, y).group(0)
-        print re.match(x, y).groupdict()
-
 MULTIPLIERS = dict([
-    #('years',  60 * 60 * 24 * 365),
-    #('months', 60 * 60 * 24 * 30),
-    ('weeks',   60 * 60 * 24 * 7),
-    ('days',    60 * 60 * 24),
-    ('hours',   60 * 60),
-    ('mins',    60),
-    ('secs',    1)
-    ])
+        #('years',  60 * 60 * 24 * 365),
+        #('months', 60 * 60 * 24 * 30),
+        ('weeks',   60 * 60 * 24 * 7),
+        ('days',    60 * 60 * 24),
+        ('hours',   60 * 60),
+        ('mins',    60),
+        ('secs',    1)
+        ])
 
 def timeparse(sval):
-    for format in TIMEFORMATS:
-        match = re.match(format + r'\s*$', sval)
+    '''
+    Parse a time expression, returning it as a number of seconds.  If
+    possible, the return value will be an `int`; if this is not
+    possible, the return will be a `float`.  Returns `None` if a time
+    expression cannot be parsed from the given string.
+
+    Arguments:
+    - `sval`: the string value to parse
+
+    >>> timeparse('1:24')
+    84
+    >>> timeparse('1 minute, 24 secs')
+    84
+    >>> timeparse('1m24s')
+    84
+    >>> timeparse('1.2 minutes')
+    72
+    >>> timeparse('1.2 seconds')
+    1.2
+    '''
+    for timefmt in TIMEFORMATS:
+        match = re.match(r'\s*' + timefmt + r'\s*$', sval, re.I)
         if match and match.group(0).strip():
             mdict = match.groupdict()
             # if all of the fields are integer numbers
